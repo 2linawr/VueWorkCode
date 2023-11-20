b71bf62869089e9aed1d722ab6a166b1b89a9e01
2linawr <61121260+2linawr@users.noreply.github.com>
2023-11-20 16:33:07 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store/index'
+import store from '/src/store/index'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
