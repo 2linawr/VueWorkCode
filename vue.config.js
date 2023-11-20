52cb9d0e0a15ee84715d66e94ef9048375821194
2linawr <61121260+2linawr@users.noreply.github.com>
2023-11-20 16:06:42 +0300

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'/TestWorkVue/',
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/something/'
+    : '/',
+    transpileDependencies: true
 })
