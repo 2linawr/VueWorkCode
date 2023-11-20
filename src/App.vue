9790fdc35e7fa20f12a0eb8139cad08d1691e081
2linawr <61121260+2linawr@users.noreply.github.com>
2023-11-20 10:05:26 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <MainPages/>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import MainPages from '../src/pages/MainPages/MainPAges';
+
+export default {
+  name: "app",
+  computed: mapGetters(["allPosts"]),
+  methods: mapActions(["fetchPosts"]),
+  components:{MainPages},
+};
+</script>
+
---
+++ src/pages/MainPages/MainPAges.vue
@@ -0,0 +1,26 @@
+<template>
+   <div className="Main">
+      <div class="block_text">
+      <span class="text_1">Жилфонд</span>
+      <span class="text_2">Пользователь</span>
+      </div>
+      <div class="block_components">
+        <SideBar/>
+        <ProfilePages/>
+        </div>
+    </div>
+</template>
+
+<script>
+import './style.scss';
+import { mapGetters, mapActions } from "vuex";
+import SideBar from '../../components/SideBar/SideBar';
+import ProfilePages from '../../components/ProfilePages/ProfilePages';
+
+export default {
+  name: "MainPages",
+  computed: mapGetters(["allPosts"]),
+  methods: mapActions(["fetchPosts"]),
+  components:{SideBar,ProfilePages},
+};
+</script>
